d29af0405920a31070c710831eb07776775fc115
AjayRavesh <ajayravesh@gmail.com>
2022-06-15 09:50:34 +0530

    completed bootstrap assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./AppComponents/MyNavbar.js";
 import MyHeader from "./AppComponents/MyHeader.js";
+import MyCard from "./AppComponents/MyCard.js";
 
 function App() {
   return (
     <div>
       <MyNavbar />
       <MyHeader />
+      <MyCard />
     </div>
   );
 }
---
+++ src/AppComponents/MyCard.js
@@ -0,0 +1,109 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+
+function MyCard() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <CardGroup>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://media.istockphoto.com/photos/close-up-of-a-buddha-figurine-and-smoky-incense-with-gong-and-picture-id1283852174?b=1&k=20&m=1283852174&s=170667a&w=0&h=6psMrfYoVh2HI9N4xjGxO8cNZh5B0OaaVy9wl12WBCo="
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Meditation edition Lord Buddha</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://images.unsplash.com/photo-1625888593861-8c7493a26025?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YnVkZGhhJTIwc3RhdHVlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Dark edition Lord Buddha </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://assets.ajio.com/medias/sys_master/root/20201201/5kga/5fc633e1aeb269d56324c235/-1117Wx1400H-461648318-green-MODEL.jpg"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Great Lord Buddha Water Fountain</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="http://cdn.home-designing.com/wp-content/uploads/2021/03/silver-buddha-statue-metallic-intricate-details-modern-spiritual-decor-for-contemporary-home-gift-inspiration-9-inch-sculpture-anjali-mudra-600x684.jpg"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Silver edition Lord Buddha </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CardGroup>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://media.istockphoto.com/photos/close-up-of-a-buddha-figurine-and-smoky-incense-with-gong-and-picture-id1283852174?b=1&k=20&m=1283852174&s=170667a&w=0&h=6psMrfYoVh2HI9N4xjGxO8cNZh5B0OaaVy9wl12WBCo="
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Meditation edition Lord Buddha</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://images.unsplash.com/photo-1625888593861-8c7493a26025?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YnVkZGhhJTIwc3RhdHVlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Dark edition Lord Buddha </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://assets.ajio.com/medias/sys_master/root/20201201/5kga/5fc633e1aeb269d56324c235/-1117Wx1400H-461648318-green-MODEL.jpg"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Great Lord Buddha Water Fountain</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="http://cdn.home-designing.com/wp-content/uploads/2021/03/silver-buddha-statue-metallic-intricate-details-modern-spiritual-decor-for-contemporary-home-gift-inspiration-9-inch-sculpture-anjali-mudra-600x684.jpg"
+              style={{ height: "18rem" }}
+            />
+            <Card.Body>
+              <Card.Title>ECRAFTINDIA</Card.Title>
+              <Card.Text>Silver edition Lord Buddha </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default MyCard;
